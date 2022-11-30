90cf8f703d19041e09e58d4876bb172a5ef473a1
Adrian Ungureanu <ady8_8@yahoo.com>
2022-11-30 09:39:57 +0200

    added aws env and updated static resources cache management

--- src/main/java/com/adrian/school_site/SpringMVCConfiguration.java
+++ src/main/java/com/adrian/school_site/SpringMVCConfiguration.java
@@ -1,6 +1,9 @@
 package com.adrian.school_site;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +15,7 @@ public class SpringMVCConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/").resourceChain(false);
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/")
+				.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS)).resourceChain(true);
 	}
 }
